09fc0a56382f8f91bb68fce2336ecfc75b987391
chaegyeong <corud9395@naver.com>
2023-04-12 10:14:20 +0900

    add3

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -5,7 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello, git!");
 		System.out.println("Nice to meet you!");
-		System.err.println("How are you today?");
+		System.out.println("How are you today?");
+		System.out.println("Great. How are you doing today?");
 		
 	}
 }
